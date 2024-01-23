c01751f35fef25f45884ebce18b23b702988b2d7
Benedictta Ogochukwu Nnaji <138885040+Dicta-og@users.noreply.github.com>
2024-01-23 15:48:45 -0800

    Add files via upload
    this is my first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn');
+const light = document.getElementById('on');
+
+function toggleEvent(){
+    btn.classList.toggle('active');
+    light.classList.toggle('on')
+
+}
+const event = document.addEventListener('btn');
